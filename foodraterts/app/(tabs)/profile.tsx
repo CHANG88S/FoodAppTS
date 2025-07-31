27942e8d62e2785ad1c9d22d8cf1fd0dfd0f4238
CHANG88S <140467098+CHANG88S@users.noreply.github.com>
2025-07-31 04:07:03 -0700

    profile pic location and possible name location
    for future to have touchableopacity to have users be able to upload profile pic to their profile

--- foodraterts/app/(tabs)/profile.tsx
+++ foodraterts/app/(tabs)/profile.tsx
@@ -1,9 +1,56 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
 
 export default function Profile() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
+    return (
+    
+        <View style={styles.root}>
+            <Image
+                source={{ uri: 'https://example.com/profile.jpg' }} 
+                style = {styles.image}>
+            </Image>
+            <Text style={styles.displayName}>
+                CHANG88S 
+            </Text>
+            <View 
+                style = {styles.center}>
+            <Text>Profile Screen</Text> 
+            </View>
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    root:{  
+        flex: 1,
+        position: 'relative',
+    },
+    container: {
+        alignContent: 'flex-start',
+        justifyContent: 'center',
+        width: 320,
+        height: 320,
+    },
+    image: {
+        width: 100,
+        height: 100,
+        marginTop: 30,      // Margins only for image
+        marginLeft: 20,     // Same Comment
+        borderColor: 'gray',
+        borderWidth: 2,
+        borderRadius: 50,   // Half of width/height for a perfect circle
+    },
+    center: {
+         flex: 4, 
+         alignItems: 'center', 
+         justifyContent: 'center' 
+    },
+    displayName: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginTop: 10,
+        color: 'black',
+        marginLeft: 20, // Align text with the image
+    }
+
+})
