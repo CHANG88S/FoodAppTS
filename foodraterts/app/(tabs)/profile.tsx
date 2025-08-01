a3852625eed2ed23fd465299586b48baa144e0de
CHANG88S <140467098+CHANG88S@users.noreply.github.com>
2025-08-01 04:01:28 -0700

    google sign-in fail
    attempted to do a google sign in implementation but failed so far

--- foodraterts/app/(tabs)/profile.tsx
+++ foodraterts/app/(tabs)/profile.tsx
@@ -6,7 +6,7 @@ export default function Profile() {
     
         <View style={styles.root}>
             <Image
-                source={{ uri: 'https://example.com/profile.jpg' }} 
+                source={{ uri: 'https://cdn.discordapp.com/attachments/1014354049333743626/1400724695866802246/Untitled.png?ex=688dae07&is=688c5c87&hm=e789a6023b4595f35753c5f68dd4b17b56c7a252c5c988f94b019273d1e9b0d5&' }} 
                 style = {styles.image}>
             </Image>
             <Text style={styles.displayName}>
@@ -46,9 +46,9 @@ const styles = StyleSheet.create({
          justifyContent: 'center' 
     },
     displayName: {
-        fontSize: 20,
+        fontSize: 18,
         fontWeight: 'bold',
-        marginTop: 10,
+        marginTop: 8,
         color: 'black',
         marginLeft: 20, // Align text with the image
     }
