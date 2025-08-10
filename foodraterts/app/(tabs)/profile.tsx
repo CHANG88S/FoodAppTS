559896f3d84a11a4694a81c06cd641e16392869b
CHANG88S <140467098+CHANG88S@users.noreply.github.com>
2025-08-10 16:44:39 -0700

    trying to fix things
    will edit more later

--- foodraterts/app/(tabs)/profile.tsx
+++ foodraterts/app/(tabs)/profile.tsx
@@ -61,23 +61,20 @@ export default function Profile() {
 
     return (
         <SafeAreaView style={styles.root}>
-                    
-                        <TouchableOpacity onPress={uploadImage}>
-                            <Image
-                                source={{ uri: 'https://cdn.discordapp.com/attachments/1014354049333743626/1400724695866802246/Untitled.png?ex=688dae07&is=688c5c87&hm=e789a6023b4595f35753c5f68dd4b17b56c7a252c5c988f94b019273d1e9b0d5&' }} 
-                            style = {styles.image}>
-                            </Image>
-                            <MaterialCommunityIcons 
-                                Name="cameraOutline"
-                                size={30}
-                                color="black"
-                                style={{ position: 'absolute', bottom: 10, right: 10 }}
-                            />
-                                <Button title="Pick an image from camera roll" onPress={pickImage} />
-                            {Image && <Image 
-                                source={{ uri: '' }} 
-                                style={styles.image} />}
-                        </TouchableOpacity>
+      <TouchableOpacity onPress={pickImage}>
+        <Image
+          source={Image ? { uri: '' } : { uri: 'https://cdn.discordapp.com/attachments/1014354049333743626/1400724695866802246/Untitled.png' }}
+          style={styles.image}
+        />
+        <MaterialCommunityIcons 
+          name="camera-outline" // Correct prop name
+          size={30}
+          color="black"
+          style={{ position: 'absolute', bottom: 10, right: 10 }}
+        />
+      </TouchableOpacity>
+      
+      <Button title="Pick an image from camera roll" onPress={pickImage} />
                             
                     <Text style={styles.displayName}>
                         CHANG88S 
