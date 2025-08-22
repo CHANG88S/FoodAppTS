cebf05b243cb788b54cbf051253c0a1c76f1ef3f
CHANG88S <140467098+CHANG88S@users.noreply.github.com>
2025-08-21 19:27:07 -0700

    empty commit
    last commit was for yesterday

--- foodraterts/app/_layout.tsx
+++ foodraterts/app/_layout.tsx
@@ -1,15 +1,14 @@
 import { Stack } from "expo-router";
 
-export default function RootLayout () {
+export default function RootLayout() {
+  const isLogin = true;
 
-    const isLogin = true;
-
-    return (
-        <Stack>
-          <Stack.Screen 
-          name = "(tabs)" // having (tabs) in the name allows it not to be displayed in the header
-          options=
-            {{ headerShown: false }}/>
-        </Stack>
-        )
-    }
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(tabs)" // having (tabs) in the name allows it not to be displayed in the header
+        options={{ headerShown: false }}
+      />
+    </Stack>
+  );
+}
