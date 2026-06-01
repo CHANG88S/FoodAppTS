7b29f23d5fb0b4b9c3fb6092489399de3f3a1b01
CHANG88S <140467098+CHANG88S@users.noreply.github.com>
2026-06-01 06:08:14 -0700

    Firebase Setup

--- foodraterts/app/(tabs)/search.tsx
+++ foodraterts/app/(tabs)/search.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native';
-import CategoryButtons from '@/components/CategoryButtons';
+// import CategoryButtons from '@/components/CategoryButtons';
 
 export default function Search() {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <CategoryButtons></CategoryButtons>
+            {/* <CategoryButtons></CategoryButtons> */}
             <Text>Search Screen</Text>
         </View>
     );
